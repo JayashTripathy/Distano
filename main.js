1c7d21274b6a89159a0142bbb02903efd90e893c
JayashTripathy <jayashtripathy371@gmail.com>
2023-04-15 19:06:30 +0530

    some changes in values

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let CANVAS;
-let ANGLE;
+let ANGLE = 0;
+let ANGLE_TO_REFERENCE_POINT = 0;
 
 
 
@@ -16,12 +17,36 @@ function main() {
 
 function onOrientationCange(event) {
   ANGLE = event.alpha;
-  const offSet = Math.PI/2;
-  const fixedAngle = ANGLE * Math.PI/ 180 + offSet
-  ;
+  const offset = -Math.PI/2;
+  const fixedAngle= (ANGLE - ANGLE_TO_REFERENCE_POINT) * Math.PI/ 180 + offset;
   const rad = Math.min(CANVAS.width, CANVAS.height) * 0.25;
 
+  const movingTip = {
+    x: CANVAS.width / 2 +  Math.cos(fixedAngle)*rad,
+    y: CANVAS.height / 2 + Math.sin(fixedAngle)*rad,
+  };
+
   const ctx = CANVAS.getContext("2d");
+   ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
+
+   
+   ctx.beginPath();
+   ctx.fillStyle = "orange";
+  
+   if(movingTip.x > CANVAS.width/2) {
+
+     ctx.arc(CANVAS.width/2, CANVAS.height/2, rad, offset, fixedAngle);
+    }else{
+      ctx.arc(CANVAS.width/2, CANVAS.height/2, rad, offset, fixedAngle, true);
+
+    }
+
+    if(movingTip.y > CANVAS.height/2) {
+      ctx.fillStyle ="red"
+    }
+   ctx.lineTo(CANVAS.width/2, CANVAS.height/2);
+   ctx.fill();
+
   ctx.beginPath();
   ctx.strokeStyle = "white";
   ctx.moveTo(CANVAS.width / 2, CANVAS.height / 2);
@@ -30,13 +55,14 @@ function onOrientationCange(event) {
 
   ctx.beginPath();
   ctx.strokeStyle = "orange";
-  const movingTip = {
-    x: CANVAS.width / 2 + rad * Math.cos(fixedAngle),
-    y: CANVAS.height / 2 - rad * Math.sin(fixedAngle),
-  };
+  
   ctx.moveTo(CANVAS.width / 2, CANVAS.height / 2);
   ctx.lineTo(movingTip.x, movingTip.y);
   ctx.stroke();
 
  
+}
+
+function reset() {
+  ANGLE_TO_REFERENCE_POINT = ANGLE;
 }
